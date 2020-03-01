ec41e3d4f031c3d2f9c493beeb0b96710a2676db
Chase.Gaddis <cgaddis36@gmail.com>
2020-03-01 12:44:57 -0700

    added in validations for numericality value of percentage and desired_quantity

--- spec/models/discount_spec.rb
+++ spec/models/discount_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Discount do
 
   describe 'Validations' do
     it {should validate_presence_of :name}
-    it {should validate_presence_of :desired_quantity}
-    it {should validate_presence_of :percentage}
+    it {should validate_numericality_of(:desired_quantity).is_greater_than(0)}
+    it {should validate_numericality_of(:percentage).is_greater_than(0).is_less_than(100)}
   end
 end
