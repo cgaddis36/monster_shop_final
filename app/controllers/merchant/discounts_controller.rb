68f52b1d2a52a80e3b7533b9c2762b5f580b1b69
Chase.Gaddis <cgaddis36@gmail.com>
2020-03-02 08:40:03 -0700

    added create method for discount to discounts controller

--- app/controllers/merchant/discounts_controller.rb
+++ app/controllers/merchant/discounts_controller.rb
@@ -2,4 +2,21 @@ class Merchant::DiscountsController < Merchant::BaseController
   def index
     @discount = Discount.new
   end
+
+  def create
+    merchant = Merchant.find(current_user.merchant_id)
+    discount =  merchant.discounts.new(discount_params)
+    if discount.save
+      redirect_to '/merchant/discounts'
+    else
+      flash[:error] = discount.errors.full_messages.to_sentence
+    end
+    require "pry"; binding.pry
+  end
+
+  private
+
+    def discount_params
+      params.permit(:name, :desired_quantity, :percentage)
+    end
 end
