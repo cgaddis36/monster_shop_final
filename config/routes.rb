61a9a3f75b0f4f3d2c9b290f7598013e7e97480d
Chase.Gaddis <cgaddis36@gmail.com>
2020-03-02 16:35:56 -0700

    added patch route to update discounts

--- config/routes.rb
+++ config/routes.rb
@@ -42,6 +42,7 @@ Rails.application.routes.draw do
     get '/orders/:id/fulfill/:order_item_id', to: 'orders#fulfill'
     get '/discounts', to: 'discounts#index'
     post '/discounts', to: 'discounts#create', as: 'create_discounts'
+    patch '/discounts/edit', to: 'discounts#update'
   end
 
   namespace :admin do
