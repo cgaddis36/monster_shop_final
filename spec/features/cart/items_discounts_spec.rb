90c4ac4307c77656f68f3fe25d086996086177fc
Chase.Gaddis <cgaddis36@gmail.com>
2020-03-03 12:18:14 -0700

    added item to other merchant for testing purposes of the discount being applied to only that merchant

--- spec/features/cart/items_discounts_spec.rb
+++ spec/features/cart/items_discounts_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'Merchant Order Show Page' do
     @merchant_2 = Merchant.create!(name: 'Tractors & Stuff', address: '125 Greenway Blvd', city: 'Denver', state: 'CO', zip: 80218)
     @item_1 = @merchant.items.create!(name: 'Skis', description: "Nice Skis!", price: 500, image: 'https://images.evo.com/imgp/700/139334/589343/clone.jpg', active: true, inventory: 5 )
     @item_2 = @merchant.items.create!(name: 'Snowboard', description: "Shred the slopes!", price: 600, image: 'https://content.backcountry.com/images/items/900/NVS/NVS008S/ONECOL.jpg', active: true, inventory: 3 )
-    @item_3 = @merchant_2.items.create!(name: 'Tractor', description: "Let me take a ride on my big green tractor", price: 50, image: 'https://target.scene7.com/is/image/Target/GUEST_a8cb0bab-7915-4ca4-b38a-8b13324f5243?wid=325&hei=325&qlt=80&fmt=webp', active: true, inventory: 3 )
+    @item_3 = @merchant_2.items.create!(name: 'Tractor', description: "Let me take a ride on my big green tractor", price: 50, image: 'https://target.scene7.com/is/image/Target/GUEST_a8cb0bab-7915-4ca4-b38a-8b13324f5243?wid=325&hei=325&qlt=80&fmt=webp', active: true, inventory: 6 )
     @bill = @merchant.users.create!(name: "Bill", address: "123 Megs Way", city: "Denver", state: "CO", zip: "12345", email: "bill@example.com", password: "bill", role: 1)
     @meredith = User.create(name: "Meredith", address: "321 Sunny Way", city: "Sunnyville", state: "FL", zip: "32541", email: "meredith@example.com", password: "meredith", role: 0)
   end
@@ -47,6 +47,9 @@ RSpec.describe 'Merchant Order Show Page' do
     click_link(@item_2.name)
     click_button("Add to Cart")
 
+    click_link(@item_3.name)
+    click_button("Add to Cart")
+
     click_link("Cart")
 
     within "#item-#{@item_1.id}" do
@@ -55,9 +58,16 @@ RSpec.describe 'Merchant Order Show Page' do
       expect(page).to have_content("Subtotal: $1,200.00")
     end
 
+    within "#item-#{@item_2.id}" do
+      expect(page).to have_content("Subtotal: $600.00")
+    end
 
-
-    save_and_open_page
+    within "#item-#{@item_3.id}" do
+      click_button("More of This!")
+      click_button("More of This!")
+      click_button("More of This!")
+      expect(page).to have_content("Subtotal: $200.00")
+    end
 
   end
 end
